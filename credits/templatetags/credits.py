81e14413196e80e1e8fb6e998ffb39d72ab8662e
Baptiste Mispelon <bmispelon@gmail.com>
2014-09-01 16:14:18 +0200

    Randomized organizers' names :)

---
+++ credits/templatetags/credits.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+from random import shuffle
+
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def organizers():
+    authors = [
+        'Baptiste Mispelon',
+        'Erik Romijn',
+        'Ola Sitarska',
+        'Marc Tamlyn',
+    ]
+    shuffle(authors)
+    return '%s, and %s' % (
+        ', '.join(authors[:-1]),
+        authors[-1]
+    )
--- duh/settings.py
+++ duh/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'interests',
     'FAQ',
+    'credits',
 )
 
 MIDDLEWARE_CLASSES = (
