4c4aca761a50235a4eec5ec4af1aabe6bd751b6d
Baptiste Mispelon <bmispelon@gmail.com>
2014-09-01 16:36:44 +0200

    Removed unused interests app.

--- interests/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Interest
-
-class InterestAdmin(admin.ModelAdmin):
-    list_display = ('email', 'name', 'message', 'created_on')
-
-
-admin.site.register(Interest, InterestAdmin)
--- interests/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-from . import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.CreateView.as_view(), name='register-interest'),
-    url(r'^(?P<token>[a-zA-Z0-9:_-]+)/$', views.UpdateView.as_view(), name='update-interest'),
-    url(r'^(?P<token>[a-zA-Z0-9:_-]+)/delete/$', views.DeleteView.as_view(), name='delete-interest'),
-)
