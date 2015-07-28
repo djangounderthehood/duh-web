e6e575542aa09e298eae9cb9f5e339621e4b2419
Baptiste Mispelon <bmispelon@gmail.com>
2015-07-28 20:23:17 +0200

    Added custom admin options for sponsors

--- sponsors/admin.py
+++ sponsors/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Sponsor
 
-admin.site.register(Sponsor)
+
+class SponsorAdmin(admin.ModelAdmin):
+    list_display = ('level', 'name', 'is_live')
+    list_filter = ('level', 'is_live')
+
+
+admin.site.register(Sponsor, SponsorAdmin)
