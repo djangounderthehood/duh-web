2d7ef25af880ffaac0bcd226c57d4e07def69954
Ola Sitarska <ola@sitarska.com>
2015-09-06 19:52:35 +0100

    Revert "Removed ticket sales link while we're sold out"
    This reverts commit ac9c9066384265de121b668346396fe61b5ca771.

--- duh/views.py
+++ duh/views.py
@@ -1,18 +1,22 @@
 from datetime import datetime
 
 from django.shortcuts import render
+from django.utils import timezone
 
 from sponsors.models import Sponsor
 from attendees.models import Attendee
 
 
 def home(request):
+    ticket_sale_start = datetime(2015, 8, 17, 15, tzinfo=timezone.utc)
     if request.user.is_staff:
         sponsors = Sponsor.objects.all()
     else:
         sponsors = Sponsor.objects.live()
     return render(request, 'home.html', {
         'sponsors': sponsors,
+        'ticket_sale_start': ticket_sale_start,
+        'now': timezone.now(),
     })
 
 def coc(request):
