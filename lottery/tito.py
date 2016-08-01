a62d8214a0eda28fc37749b49ed176c3fd836ebc
Ola Sitarska <ola@sitarska.com>
2016-08-01 17:04:50 +0100

    Update exporting Tito CSV with a newest template

--- lottery/tito.py
+++ lottery/tito.py
@@ -1,11 +1,13 @@
 import tablib
 
-PERSONAL_COLUMNS = ['First Name', 'Last Name', 'Email', 'Expiry time', 'Redirect?']
+PERSONAL_COLUMNS = ['First Name', 'Last Name', 'Email', 'Expiry time', 'Redirect?', 'Discount code']
 
 # TODO: Check we've got *all* tickets named here
 # Tito importer will protest if we don't list all current tickets categories
-# Don't change order of individual and corporate tickets
-TICKET_COLUMNS = ['Individual Ticket', 'Corporate Ticket', 'Scholarship Ticket']
+# Don't change order of individual, corporate and donation tickets
+TICKET_COLUMNS = ['Individual Ticket', 'Corporate Ticket', 'Donation to Scholarship Programme',
+    'Scholarship Ticket', 'Speaker Ticket', 'Organizer Ticket', 'Core Team Ticket', 'Sponsor Ticket', 
+    'Staff Ticket']
 
 
 def export_participants_to_csv(participants):
@@ -18,13 +20,13 @@ def export_participants_to_csv(participants):
     data = tablib.Dataset(headers=headers)
     for p in participants:
         # Personal columns
-        row = [p.first_name, p.last_name, p.email, p.batch.expires_at, 'Y']
+        row = [p.first_name, p.last_name, p.email, p.batch.expires_at, 'Y', '']
 
-        # Individual and corporate tickets
-        row += ['Y' if p.individual else '', 'Y' if p.corporate else '']
+        # Individual, corporate and donation tickets
+        row += ['Y' if p.individual else '', 'Y' if p.corporate else '', 'Y']
 
         # Rest of the tickets
-        row += [''] * (len(TICKET_COLUMNS) - 2)
+        row += [''] * (len(TICKET_COLUMNS) - 3)
 
         data.append(row)
 
